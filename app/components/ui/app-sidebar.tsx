41dcdcbf7fe2c78b64d91bb6201792af85a1b161
Deepak <deepudeepz383@gmail.com>
2025-12-15 20:06:36 +0530

    profile section updated

--- app/components/ui/app-sidebar.tsx
+++ app/components/ui/app-sidebar.tsx
@@ -79,17 +79,6 @@ export function AppSidebar() {
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarFooter>
-      <form
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: "/" });
-        }}
-      >
-        <button className="flex w-full h-12 grow items-center justify-center gap-2 rounded-md bg-gray-500 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-          <PowerIcon className="w-6" />
-          <div className="block">Sign Out</div>
-        </button>
-      </form>
     </Sidebar>
   );
 }
